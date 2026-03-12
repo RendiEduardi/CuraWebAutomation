74a13c3b3d1ed36dc759d791c1f555e1be7013bd
RENDI\rendy <rendy.eduardi@gmail.com>
2026-03-12 14:04:39 +0700

    edit source code

--- src/test/java/stepdefinitions/LoginStep.java
+++ src/test/java/stepdefinitions/LoginStep.java
@@ -26,7 +26,7 @@ public class LoginStep {
         driver.findElement(By.id("txt-username")).clear();
         driver.findElement(By.id("txt-username")).sendKeys(username);
         driver.findElement(By.id("txt-password")).clear();
-        driver.findElement(By.id("txt-password")).sendKeys(password); //fix error untuk cek log di jenkins
+        driver.findElement(By.id("txt-password")).sendKeys(password); //fixing error untuk cek log di jenkins
         System.out.println("Username dan password berhasil diinput");
     }
 
